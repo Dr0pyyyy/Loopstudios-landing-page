adbba5e8e284b1b3cba11c6402e03df59a5eb079
Dr0pyyyy <adamkoutny12345seznam.cz>
2023-04-01 22:03:47 +0200

    Adding js control on menu

---
+++ header.js
@@ -0,0 +1,16 @@
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const headerContent = document.querySelector("header .content");
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+
+hamburgerIcon.addEventListener("click", function () {
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+        header.style.backgroundImage = "url(\"./images/mobile/image-hero.jpg\")";
+        headerContent.style.display = "block";
+    } else {
+        nav.style.display = "block";
+        header.style.backgroundImage = "none";
+        headerContent.style.display = "none";
+    }
+});
